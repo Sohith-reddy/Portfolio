af5e0178a152f6f5e82edada36a447b5648746f9
SohithReddy <sohithreddy33@gmail.com>
2025-03-16 02:02:31 +0530

    Initial Commit

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Outfit as OutfitFont, Ovo as OvoFont } from "next/font/google";
+import "./globals.css";
+
+const outfit = OutfitFont({
+  subsets: ["latin"],
+  weight:["400","500","600","700"]
+});
+
+const ovo = OvoFont({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+
+export const metadata = {
+  title: "Portfolio",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
